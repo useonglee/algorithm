48ca80c2ed061e18a0c85ce4c8e3e7981e8cbf78
useonglee <useong0830@naver.com>
2021-09-24 23:57:50 +0900

    TestDome: JS - 09.Two Sum

---
+++ TestDome/09-TwoSum.js
@@ -0,0 +1,11 @@
+// TODO: TestDome - Two Sum
+// https://www.testdome.com/questions/56606
+
+function findTwoSum(numbers, sum) {
+  // 만약 조건에 맞는게 없다면 Null을 반환.
+  // 요소 두개를 선택해서 sum과 같으면 한 쌍이다.
+  // 
+}
+
+const indices = findTwoSum([ 3, 1, 5, 7, 5, 9 ], 10);
+console.log(indices);
